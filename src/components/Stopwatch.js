bc02079add5ac0ac58126baf272f8a3c7ff6e09a
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-08-08 14:48:08 -0400

    ..

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,11 +1,14 @@
 import React,  {useState} from "react";
 
 const Stopwatch = () => {
+    const [isRunning, setIsRunning] = useState(false);
     return(
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span  className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={()=> setIsRunning((prevValue)=> !prevValue )}>
+            {isRunning? "stop" : "start"}
+        </button>
         <button>Reset</button>
       </div>
     )
